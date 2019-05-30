b29cc78a3c2e0a06f714306d44dec408c8f07ce4
Rahmanism <s.mostafa.rahmani@gmail.com>
2019-05-30 18:31:41 +0430

    rm --cached the secrets!

--- gettweets_secret_keys.py
+++
@@ -1,8 +0,0 @@
-# put twitter consumer secret keys here...
-
-# App use description: Your Use Description!!
-# API key: yourapikeyoi34u759847tkdfjhgeiroytu
-# API secret key: yourapisecretkey2398457dhfj384957uoerhhg 
-# Access token: anaccesstokenfullofj2347adoi23uy489dkfjgh38945y
-# Access token secret: asecretaccesstokenfullof23894789andksdjfghaqleu
-# Dev environment label: somecodefordevenvironmentlabel
