4e79e0f9d44e65343c3eab3f805527da201ef56d
rahmanism <s.mostafa.rahmani@gmail.com>
2020-11-18 22:33:56 -0800

    Add run_fetch.cmd to use in schedule.

---
+++ run_fetch.cmd
@@ -0,0 +1,5 @@
+﻿python gettweets_api.py -hg زندگی -c 2000
+python gettweets_api.py -hg تاریخ -c 2000
+python gettweets_api.py -hg فرهنگ -c 2000
+del r.csv
+python gettweets_api.py -x r.csv -xc 6000
